46babd3b96692326d9cbda7d970347840b18a50a
bobbyhadz <opmftw@gmail.com>
2024-04-06 16:07:49 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <h2>bobbyhadz.com</h2>
+    </div>
+  );
+};
+
+export default App;
